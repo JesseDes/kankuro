e4d17c9910a70601dfea0aee1a35d30d673dc232
jesse <desmaraisjesse@gmail.com>
2020-01-24 21:13:01 -0500

    Sample MVC
    Added a sample button that uses the MVC approach explained in class

---
+++ src/src/entities/SampleMVCButton/SampleButtonView.java
@@ -0,0 +1,11 @@
+package entities.SampleMVCButton;
+
+import javax.swing.JButton;
+
+public class SampleButtonView extends JButton  {
+
+	public SampleButtonView(String buttonText)
+	{
+		this.setText(buttonText);
+	}
+}
